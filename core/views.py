7f205fa2022e39cfee65c5afa02af07479b745bd
Inorro <ronimujku@gmail.com>
2024-01-09 14:01:56 +0000

    a

--- core/views.py
+++ core/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    #return HttpResponse('Welcome')
-    pass
+    return render(request, "core/index.html")
+
+def about(request):
+    return render(request, "core/About.html")
+
+def booking(request):
+    return render(request, "core/Bookin.html")
+
+def socialmedia(request):
+    return render(request, "core/Social Media.html")
--- webproj/urls.py
+++ webproj/urls.py
@@ -17,9 +17,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index
+from core.views import index,booking,about,socialmedia
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", index)
+    path("", index),
+    path("", booking),
+    path("", about),
+    path("", socialmedia)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
