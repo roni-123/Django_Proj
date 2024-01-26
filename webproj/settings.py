0dfc223cc40b461386963c961bc6155aada6ead6
Inorro <ronimujku@gmail.com>
2024-01-26 10:32:34 +0000

    ESR NATION SHALL RISE

--- webproj/settings.py
+++ webproj/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'webproj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webdata',
+        'USER': 'root',
+        'PASSWORD': 'Roni_Mujku1',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
