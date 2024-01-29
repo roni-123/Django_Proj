a03713c7245a231fea5afd943ffc869e4a8b86d7
Inorro <ronimujku@gmail.com>
2024-01-29 16:29:29 +0000

    Fixed databasing

--- core/models.py
+++ core/models.py
@@ -3,5 +3,14 @@ from django.db import models
 # Create your models here.
 
 class Members(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
+    fname = models.CharField(max_length=50, default='')
+    email = models.EmailField(default='')
+    password = models.CharField(max_length=50, default='')
+    conpassword = models.CharField(max_length=50, default='')
+
+    class Meta:
+        verbose_name = 'Member'
+        verbose_name_plural = 'Members'
+
+    def __str__(self):
+        return self.email
--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from .models import Members
 # Create your views here.
 
 def index(request):
+    if request.method == "POST":
+        fname = request.POST["fname"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        conpassword = request.POST["conpassword"]
+
+        new_member = Members(fname = fname, email = email,password = password,conpassword = conpassword)
+        new_member.save()
+
     return render(request, "core/index.html")
 
 def about(request):
