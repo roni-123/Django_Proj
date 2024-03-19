096849702cba3c6fb92d0b0b39474fbc39a60e19
Inorro <ronimujku@gmail.com>
2024-03-19 12:52:04 +0000

    s

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Booking,Classes,Menu
+from .models import Booking,Classes,Menu,MyUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    list_display=['first_name','email']
+    list_filter = ('is_staff', 'is_superuser', 'groups')
+
 
 # Register your models here.
 admin.site.register(Booking)
 admin.site.register(Classes)
 admin.site.register(Menu)
+admin.site.register(MyUser, UserAdmin)
--- core/models.py
+++ core/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django_mysql.models import ListTextField
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,PermissionsMixin
 
 # Create your models here.
 
@@ -56,7 +56,6 @@ class Menu(models.Model):
     def __str__(self):
         return self.email
     
-    
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password,first_name,last_name):
         if not email:
@@ -72,23 +71,26 @@ class CustomUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password,first_name,last_name):
+    def create_superuser(self, email, password):
         user = self.create_user(
             email=email,
-            first_name = first_name,
-            last_name = last_name,
             password=password,
+            first_name="",
+            last_name=""
         )
         user.is_admin = True
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
         return user
     
-class MyUser(AbstractBaseUser):
+class MyUser(AbstractBaseUser,PermissionsMixin):
     email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
-
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
@@ -96,3 +98,9 @@ class MyUser(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+    
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
